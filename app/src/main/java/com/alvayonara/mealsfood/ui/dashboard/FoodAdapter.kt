0cd31f7d9daa482be50954cc2b22c6ddf53e9170
Alva Yonara Puramandya <alvayonara@outlook.com>
2020-10-28 17:47:03 +0700

    Update FoodAdapter.kt

--- app/src/main/java/com/alvayonara/mealsfood/ui/dashboard/FoodAdapter.kt
+++ app/src/main/java/com/alvayonara/mealsfood/ui/dashboard/FoodAdapter.kt
@@ -1,11 +1,17 @@
 package com.alvayonara.mealsfood.ui.dashboard
 
+import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.alvayonara.mealsfood.FoodEntity
 import com.alvayonara.mealsfood.R
+import com.alvayonara.mealsfood.ui.detail.DetailFoodFragment
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.item_row_food.view.*
+
 
 class FoodAdapter : RecyclerView.Adapter<FoodAdapter.FoodViewHolder>() {
 
@@ -32,7 +38,33 @@ class FoodAdapter : RecyclerView.Adapter<FoodAdapter.FoodViewHolder>() {
         fun bindItem(food: FoodEntity) {
             with(itemView) {
                 food.let {
+                    Glide.with(context)
+                        .load(it.foodThumb)
+                        .into(iv_food)
 
+                    tv_food.text = it.foodName
+
+                    setOnClickListener {
+                        val mBundle = Bundle().apply {
+                            putString(DetailFoodFragment.EXTRA_FOOD_ID, food.foodId)
+                        }
+
+                        val mDetailFoodFragment = DetailFoodFragment().apply {
+                            arguments = mBundle
+                        }
+
+                        val mFragmentManager =
+                            (context as AppCompatActivity).supportFragmentManager
+                        mFragmentManager.beginTransaction().apply {
+                            replace(
+                                R.id.frame_dashboard,
+                                mDetailFoodFragment,
+                                DetailFoodFragment::class.java.simpleName
+                            )
+                            addToBackStack(null)
+                            commit()
+                        }
+                    }
                 }
             }
         }
